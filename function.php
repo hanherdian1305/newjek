774a9d56affb98beffd8f2b048c0caa0cb2f3f27
hanherdian1305 <hanherdian1305@gmail.com>
2020-06-26 10:09:37 +0700

    function.php

--- function.php
+++ function.php
@@ -11,7 +11,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.904028,107.621092";
+$header[] = "X-Location: -6.262834,106.977381";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
